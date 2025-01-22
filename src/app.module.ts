2cf032f2088941612b1f824cba34fc771a52d6c3
Nabeel Noor <nabeelnoor914@gmail.com>
2025-01-23 04:22:58 +0500

    fix: implement cache service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,31 @@ import { ConfigModule } from '@nestjs/config';
 
 import configuration from 'config/configuration';
 import { PlayersModule } from 'src/modules/players/players.module';
-
+// import { CacheModule } from 'src/modules/cache/cache.module';
+// import * as config from 'config';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
 @Module({
   imports: [
     PlayersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      useFactory: async () => {
+        return {
+          stores: [new KeyvRedis('redis://localhost:6378')],
+        };
+      },
+      isGlobal: true,
+    }),
+    // CacheModule.forRoot({
+    //   endPoint: 'localhost',
+    //   port: +'6378',
+    //   redisPrefix: 'dev',
+    //   password: '123456',
+    //   username: 'default',
+    // }),
     ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
   ],
   controllers: [],
--- src/modules/players/service/players.service.ts
+++ src/modules/players/service/players.service.ts
@@ -1,12 +1,18 @@
-import { Logger } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Logger } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 export class PlayersService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
   private readonly logger = new Logger(PlayersService.name);
 
   async findMatches() {
     try {
       this.logger.log('testing');
-      return { matches: [] };
+      // await this.cacheManager.set('test', { key: 'abc' }, 0);
+      const data = await this.cacheManager.get('test');
+
+      return { matches: [], data };
     } catch (err) {
       this.logger.error(err);
     }
